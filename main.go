d37e23daf8e39b5cf6877ac6c25ad1c2cdeb07ca
Mountains-and-rivers <qzrkqzry2017@gmail.com>
2021-08-01 19:17:51 +0800

    1

--- main.go
+++ main.go
@@ -1,73 +1,68 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/bitly/go-simplejson"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"io/ioutil"
+	"log"
 	"net/url"
 )
 
-func proxy_iocc()  {
-	// Echo instance
+func proxy_iocc(port string,proxyurl string) {
 	e := echo.New()
-	// Routes
-	//e.Static("/static", "static")
-	//e.File("/", "templates/login.html")
-	// Setup proxy
-	url1, err := url.Parse("http://localhost:9001")
+	url1, err := url.Parse(proxyurl)
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
-	//url2, err := url.Parse("http://localhost:8082")
-	//if err != nil {
-	//	e.Logger.Fatal(err)
-	//}
 	targets := []*middleware.ProxyTarget{
 		{
 			URL: url1,
 		},
-		//{
-		//	URL: url2,
-		//},
 	}
 	e.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
-	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func proxy_iocc1()  {
-	// Echo instance
-	e := echo.New()
-	// Routes
-	e.Static("/static", "static")
-	e.File("/1", "templates/login.html")
-	// Setup proxy
-	url1, err := url.Parse("http://localhost:9001")
-	if err != nil {
-		e.Logger.Fatal(err)
-	}
-	//url2, err := url.Parse("http://localhost:8082")
-	//if err != nil {
-	//	e.Logger.Fatal(err)
-	//}
-	targets := []*middleware.ProxyTarget{
-		{
-			URL: url1,
-		},
-		//{
-		//	URL: url2,
-		//},
-	}
-	e.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
-	// Start server
-	e.Logger.Fatal(e.Start(":8001"))
+	fmt.Println(port)
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
 func main() {
-	go proxy_iocc()
-	go proxy_iocc1()
-	e := echo.New()
-	// Routes
-	e.Static("/static", "static")
-	e.File("/1", "templates/login.html")
-	e.Logger.Fatal(e.Start(":2323"))
+	//type hostconfig struct {
+	//	IP string `json:"ip"`
+	//	//HOSTNAME string `json:"hostname"`
+	//}
+	//
+	//type hostconfigs struct {
+	//	IP1   map[string]hostconfig   `json:"ip1"`
+	//	IP2   map[string]hostconfig   `json:"ip2"`
+	//	IP3   map[string]hostconfig   `json:"ip3"`
+	//}
+	//var hconfig hostconfigs
+	//portArr := []string{"8001", "8002", "8003", "8004", "8005","8006"}
+	plan, err := ioutil.ReadFile("conf/config.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	buf := bytes.NewBuffer(plan)
+	res, err := simplejson.NewFromReader(buf)
+	if err != nil || res == nil {
+		log.Fatal("something wrong when call NewFromReader")
+	}
+	confArr, _ := res.Get("proxy_arr").Array()
+	for _, row := range confArr {
+		if each_map, ok := row.(map[string]interface{}); ok {
+			proxy_IP := each_map["IP"].(string)
+			proxy_port := each_map["proxy_port"].(string)
+			go proxy_iocc(proxy_port,proxy_IP)
+		}
+	}
+		e := echo.New()
+		e.Static("/static", "static")
+		e.File("/", "templates/login.html")
+		e.GET("/config", func(c echo.Context) error {
+			u := res
+			return c.JSON(200, u)
+		})
+		e.Logger.Fatal(e.Start(":2323"))
 }
